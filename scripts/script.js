6ca59829423cb51eaa977057d6a83a4b7efc712c
NikSkor <mr-zerx@yandex.ru>
2022-10-13 03:31:11 +0500

    self-employment calc done

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+// const formatCurrency = (number) => {
+//   const currency = new Intl.NumberFormat('ru-RU', {
+//     style: 'currency',
+//     currency: 'RUB',
+//     maximumFractionDigits: 2,
+//   });
+
+//   return currency.format(number);
+// }
+
+const formatCurrency = number =>
+  new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    maximumFractionDigits: 2,
+  }).format(number);
+
+
 const navigationLinks = document.querySelectorAll('.navigation__link');
 const calcElems = document.querySelectorAll('.calc')
 
@@ -46,13 +64,13 @@ calcLabelExpenses.style.display = 'none';
 formAusn.addEventListener('input', () => {
   if(formAusn.type.value === 'income') {
     calcLabelExpenses.style.display = 'none';
-    resultTaxTotal.textContent = formAusn.income.value * 0.08
+    resultTaxTotal.textContent = formatCurrency(formAusn.income.value * 0.08);
     formAusn.expenses.value = '';
   }
 
   if(formAusn.type.value === 'expenses') {
     calcLabelExpenses.style.display = 'block';
-    resultTaxTotal.textContent = ((formAusn.income.value - formAusn.expenses.value) * 0.2).toFixed(2);
+    resultTaxTotal.textContent = formatCurrency((formAusn.income.value - formAusn.expenses.value) * 0.2);
   }
 });
 
@@ -61,10 +79,54 @@ formAusn.addEventListener('input', () => {
 const selfEmployment = document.querySelector('.self-employment');
 const formSelfEmployment = selfEmployment.querySelector('.calc__form');
 const resultTaxTotalSelfEmployment = selfEmployment.querySelector('.result__tax_total');
+const calcCompensation = selfEmployment.querySelector('.calc__label_compensation');
+const resultBlockCompensation = selfEmployment.querySelectorAll('.result__block_compensation');
+
+const resultTaxCompensation = selfEmployment.querySelector('.result__tax_compensation');
+const resultTaxRestCompensation = selfEmployment.querySelector('.result__tax_rest-compensation');
+const resultTaxResult = selfEmployment.querySelector('.result__tax_result');
+
+const checkCompensation = () => {
+  const setDisplay = formSelfEmployment.addCompensation.checked
+    ? 'block'
+    : 'none';
+  calcCompensation.style.display = setDisplay;
+
+  if (!formSelfEmployment.addCompensation.checked) {
+    formSelfEmployment.compensation.value = '';
+  }
+
+  resultBlockCompensation.forEach((elem) => {
+    elem.style.display = setDisplay;
+  })
+}
+
+checkCompensation();
 
 formSelfEmployment.addEventListener('input', () => {
-  resultTaxTotalSelfEmployment.textContent = ((formSelfEmployment.psysicalPerson.value * 0.04)
-    + (formSelfEmployment.legalPerson.value * 0.06)).toFixed(2);
+  const resPsysicalPerson = formSelfEmployment.psysicalPerson.value * 0.04;
+  const resLegalPerson = formSelfEmployment.legalPerson.value * 0.06;
+
+  checkCompensation();
+
+  const tax = resPsysicalPerson + resLegalPerson;
+  formSelfEmployment.compensation.value =
+    formSelfEmployment.compensation.value > 10_000
+    ? 10_000
+    : formSelfEmployment.compensation.value;
+
+  const benefit = formSelfEmployment.compensation.value;
+  const resBenefit = formSelfEmployment.psysicalPerson.value * 0.01
+    + formSelfEmployment.legalPerson.value * 0.02;
+  const finalBenefit = benefit - resBenefit > 0 ? benefit - resBenefit : 0;
+  const finalTax = tax - (benefit - finalBenefit);
+
+  resultTaxTotalSelfEmployment.textContent = formatCurrency(tax);
+
+  resultTaxCompensation.textContent = formatCurrency(benefit - finalBenefit);
+  resultTaxRestCompensation.textContent = formatCurrency(finalBenefit);
+  resultTaxResult.textContent = formatCurrency(finalTax);
+
 });
 
 
